e6bb48a8fe676df87276533d1b3c932cc0ae2674
Bae Ji-min <dncl46@gmail.com>
2024-06-11 15:33:16 +0900

    graphql tools modularization

---
+++ comments/comments.model.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    id: 'comment1',
+    text: 'It is a first comment',
+    likes: 1
+  },
+  {
+      id: 'comment2',
+      text: 'It is a second comment',
+      likes: 0
+  },
+  {
+      id: 'comment3',
+      text: 'It is a third comment',
+      likes: 5
+  }
+]
---
+++ posts/posts.model.js
@@ -0,0 +1,28 @@
+module.exports = [
+  {
+      id: 'post1',
+      title: 'Ii is a first post',
+      description: 'It is a first post descrition',
+      comments: [{
+          id: 'comment1',
+          text: 'It is a first comment',
+          likes: 1
+      }]
+  },
+  {
+      id: 'post2',
+      title: 'Ii is a second post',
+      description: 'It is a second post descrition',
+      comments: []
+  },
+  {
+      id: 'post3',
+      title: 'Ii is a third post',
+      description: 'It is a third post descrition',
+      comments: [{
+          id: 'comment3',
+          text: 'It is a third comment',
+          likes: 5
+      }]
+  }
+]
